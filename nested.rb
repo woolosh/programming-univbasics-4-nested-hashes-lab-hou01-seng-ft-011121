facbb16757eadfc110e31273676f90172445a5f2
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 19:00:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,9 +96,10 @@ def adding_matz
     :languages => ["LISP", "C"]
   }
   
-   programmer_hash
+   return programmer_hash
    
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
