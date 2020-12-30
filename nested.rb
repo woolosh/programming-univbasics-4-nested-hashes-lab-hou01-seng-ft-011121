ce82bfd569a9e0631acc88a40996e624c30a60ff
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 19:07:00 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,10 @@ def hopper
       }
     }
 
+return programmer_hash[:grace_hopper]
+
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,7 +40,10 @@ def alan_kay_is_known_for
       }
     }
 
+return programmer_hash[:alan_kay][:known_for]
+
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -56,7 +62,10 @@ def dennis_ritchies_language
       }
     }
 
+return programmer_hash[:dennis_ritchie][:languages][0]
+
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -81,8 +90,16 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  
+   return programmer_hash
+   
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -104,7 +121,12 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+
+return programmer_hash
+
 end
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
@@ -125,5 +147,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+
+return programmer_hash
 
 end
