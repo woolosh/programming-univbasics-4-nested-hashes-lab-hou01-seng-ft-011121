99861460d7bb6c2ee92f21dfbb2be2b1530a7672
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 18:52:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,10 @@ def dennis_ritchies_language
       }
     }
 
+return programmer_hash[:dennis_ritchie][:languages][0]
+
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
