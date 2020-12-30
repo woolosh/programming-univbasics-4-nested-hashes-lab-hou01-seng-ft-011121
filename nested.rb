6c5c9b9d6a5f0be05b7f3a0b5671061645347eb6
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 19:06:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,7 @@ return programmer_hash
 
 end
 
+
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
@@ -146,5 +147,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+
+return programmer_hash
 
 end
